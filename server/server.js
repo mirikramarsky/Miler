650e34cba00af7c9388266c711ebdc743ce7a249
mirikramarsky <talmida.tichnut.miriam.karmarsky@byb.org.il>
2025-09-09 11:23:48 +0300

    add changes

--- server/server.js
+++ server/server.js
@@ -67,10 +67,10 @@ ${items}
   });
 });
 // משרת את קבצי Angular סטטיים
-app.use(express.static(path.join(__dirname, '../client2/dist')));
+app.use(express.static(path.join(__dirname, '../client2/dist/client2')));
 // כל בקשה שלא תואמת ל-route בשרת תוחזר ל-index.html
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../client2/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../client2/dist/client2/index.html'));
 });
 
 
