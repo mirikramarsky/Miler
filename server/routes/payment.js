9ad9aad898910777a9309b1b55bdf14197168f48
mirikramarsky <talmida.tichnut.miriam.karmarsky@byb.org.il>
2025-09-27 22:21:19 +0300

    find the buyng items

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -217,7 +217,7 @@ router.post("/create", async (req, res) => {
       KEY: process.env.HYP_KEY,
       action: "APISign",
       What: "SIGN",
-      PassP: "yaad.net",
+      PassP: yaad.net,
       Order: ordernum,
       Masof: process.env.HYP_TERMINAL,
       Info: "רכישה באתר מילר סטנדרים",
